719f240b0c1489819e7b41b5d085510b6f920a76
Paul McLanahan <pmac@mozilla.com>
2016-08-17 12:28:35 -0400

    Add git-push ability to shell script

--- update-and-push.sh
+++ update-and-push.sh
@@ -4,3 +4,9 @@ IMAGE_NAME="${DOCKER_IMAGE_NAME:-product-details-json}"
 docker build -t "$IMAGE_NAME" --pull .
 docker run -v "$PWD/product-details:/app/product-details" "$IMAGE_NAME"
 
+if [[ "$1" == "push" ]]; then
+    git add ./product-details/
+    if git commit -m "Update product-details data"; then
+        git push origin master
+    fi
+fi
