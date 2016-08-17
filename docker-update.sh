22359b70b53d3ac442fa5287ae06dc040e014bd0
Paul McLanahan <pmac@mozilla.com>
2016-08-17 14:54:59 -0400

    Create a file when the update script runs
    This will be used by Jenkins to fire off other
    jobs if an update occurred.

--- docker-update.sh
+++ docker-update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -ex
 
+UPDATE_FILE=".new-revision-pushed"
 IMAGE_NAME="${DOCKER_IMAGE_NAME:-product-details-json}"
 docker build -t "$IMAGE_NAME" --pull .
 docker run -v "$PWD/product-details:/app/product-details" "$IMAGE_NAME"
+
+# UPDATE_FILE is an indicator we can use in Jenkins
+# to run other jobs if there was, in fact, an update
+rm "$UPDATE_FILE" || true
 
 if [[ "$1" == "push" ]]; then
     git add ./product-details/
     if git commit -m "Update product-details data"; then
         git push origin master
+        git rev-parse HEAD > "$UPDATE_FILE"
     fi
 fi
