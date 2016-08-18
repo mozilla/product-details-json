9806b6ff88d68e57519005024b9162e693065359
Paul McLanahan <pmac@mozilla.com>
2016-08-18 16:48:43 -0400

    Fix git branch and push

--- docker-update.sh
+++ docker-update.sh
@@ -2,6 +2,10 @@
 
 UPDATE_FILE=".new-revision-pushed"
 IMAGE_NAME="${DOCKER_IMAGE_NAME:-product-details-json}"
+GIT_BRANCH="jenkins-update-pd"
+
+git branch -D "$GIT_BRANCH" || true
+git checkout -b "$GIT_BRANCH"
 docker build -t "$IMAGE_NAME" --pull .
 docker run -v "$PWD/product-details:/app/product-details" "$IMAGE_NAME"
 
@@ -10,10 +14,9 @@ docker run -v "$PWD/product-details:/app/product-details" "$IMAGE_NAME"
 rm -f "$UPDATE_FILE"
 
 if [[ "$1" == "push" ]]; then
-    git checkout master
     git add ./product-details/
     if git commit -m "Update product-details data"; then
-        git push origin master
+        git push origin "$GIT_BRANCH:master"
         git rev-parse HEAD > "$UPDATE_FILE"
     fi
 fi
