e4addb55eace4d7cd2423dca5a8e4953d2f36a5c
Paul McLanahan <pmac@mozilla.com>
2016-08-18 22:03:55 -0400

    Improve dockerfile and let Jenkins git push

--- docker-update.sh
+++ docker-update.sh
@@ -2,10 +2,7 @@
 
 UPDATE_FILE=".new-revision-pushed"
 IMAGE_NAME="${DOCKER_IMAGE_NAME:-product-details-json}"
-GIT_BRANCH="jenkins-update-pd"
 
-git branch -D "$GIT_BRANCH" || true
-git checkout -b "$GIT_BRANCH"
 docker build -t "$IMAGE_NAME" --pull .
 docker run -v "$PWD/product-details:/app/product-details" "$IMAGE_NAME"
 
@@ -13,10 +10,14 @@ docker run -v "$PWD/product-details:/app/product-details" "$IMAGE_NAME"
 # to run other jobs if there was, in fact, an update
 rm -f "$UPDATE_FILE"
 
-if [[ "$1" == "push" ]]; then
+if [[ "$1" == "commit" ]]; then
     git add ./product-details/
     if git commit -m "Update product-details data"; then
-        git push origin "$GIT_BRANCH:master"
         git rev-parse HEAD > "$UPDATE_FILE"
+        echo "Product-details update committed"
+        exit 0
+    else
+        echo "No new product-details"
+        exit 1
     fi
 fi
