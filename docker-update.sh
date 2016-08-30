0c051613a3f0cff7fe16a0a67c932f668a5b7252
Josh Mize <jgmize@gmail.com>
2016-08-30 10:54:16 -0500

    Add Dockerfile for serving json

--- docker-update.sh
+++ docker-update.sh
@@ -3,7 +3,7 @@
 UPDATE_FILE=".new-revision-pushed"
 IMAGE_NAME="${DOCKER_IMAGE_NAME:-product-details-json}"
 
-docker build -t "$IMAGE_NAME" --pull .
+docker build -f Dockerfile-update -t "$IMAGE_NAME" --pull .
 docker run -v "$PWD/product-details:/app/product-details" "$IMAGE_NAME"
 
 # UPDATE_FILE is an indicator we can use in Jenkins
